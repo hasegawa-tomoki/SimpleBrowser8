9274b64dffb4629c52ba9b60167bd4d595696da5
hasegawa.tomoki <github@speedstars.jp>
2017-02-04 22:19:13 +0900

    Chapter 03-01

---
+++ SimpleBrowserTests/SimpleBrowserTests.swift
@@ -0,0 +1,36 @@
+//
+//  SimpleBrowserTests.swift
+//  SimpleBrowserTests
+//
+//  Created by 長谷川智希 on 2017/02/04.
+//  Copyright © 2017年 長谷川智希. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleBrowser
+
+class SimpleBrowserTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
